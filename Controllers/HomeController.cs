b8fb88f3ffcffe82c4aa8b4fe552fca0a1a16517
JONATAN NAIFELD <49549062@ort>
2025-04-30 08:49:50 -0300

    controller terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,18 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        empresa.InicializarCatalogo();
+        ViewBag.catalogo = empresa.catalogo;
+        return View();
+    }
+     public IActionResult infoDisco(int id)
+    {
+        empresa.InicializarCatalogo();
+        int i = -1;
+        do{
+            i++;
+        }while(empresa.catalogo[i].id != id && id < empresa.catalogo.Count);
+        if(i < empresa.catalogo.Count) ViewBag.disco = empresa.catalogo[i];
         return View();
     }
 }
